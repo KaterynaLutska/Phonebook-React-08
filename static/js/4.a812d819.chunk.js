e7bb8106b6d6fa142a0a121800ee71694f776c48
Kateryna Lutska <ket.lutska@gmail.com>
2021-04-18 10:22:42 +0300

    Updates

---
+++ static/js/4.a812d819.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[4],{116:function(t,e,n){"use strict";n.r(e);var o=n(1);e.default=function(){return Object(o.jsx)("div",{children:Object(o.jsx)("h1",{children:"It is your best Phonebook ever"})})}}}]);
+//# sourceMappingURL=4.a812d819.chunk.js.map
